9756447e64ecc21c1fc288cba810d55e25288857
bruno.rodrigues <bruno.rodrigues@datahub.com.br>
2023-10-20 20:03:56 -0300

    ratoração do codigo

--- tests/teste.py
+++ tests/teste.py
@@ -1,5 +1,5 @@
-from utils import restaurante as rt
-from utils import menu
+from utils.restaurante import restaurante as rt
+from utils.menu import menu
 
 print(f"A distância entre São Paulo e Rio de janeiro em Km é: {rt.proximidade_restaurante_geopy((23.550278, -46.633889), (22.902778, -43.207778))}")
 print(f"A distância entre São Paulo e Rio de janeiro em Km é: {rt.proximidade_restaurante((23.550278, -46.633889), (22.902778, -43.207778))}")

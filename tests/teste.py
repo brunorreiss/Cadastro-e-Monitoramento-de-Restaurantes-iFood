4ca5ee0baf84aeb06c916f44fd7958a033567a90
Matheus Acer <matheusmibrandao@gmail.com>
2023-10-21 10:55:52 -0300

    Refactor

--- tests/teste.py
+++ tests/teste.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.insert(1, '../')
 
-from utils.restaurante import restaurante as rt
-from utils.menu import menu
+from src.restaurante import restaurante as rt
+from src.menu import menu
 
 print(f"A distância entre São Paulo e Rio de janeiro em Km é: {rt.proximidade_restaurante_geopy((23.550278, -46.633889), (22.902778, -43.207778))}")
 print(f"A distância entre São Paulo e Rio de janeiro em Km é: {rt.proximidade_restaurante((23.550278, -46.633889), (22.902778, -43.207778))}")

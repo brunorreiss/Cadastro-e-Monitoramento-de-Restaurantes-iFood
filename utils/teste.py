e72a597096600fb2396dbb30d03f71e7b90cefae
gabriel victor <gabrielvmdovital@gmail.com>
2023-10-20 22:28:38 -0300

    adicionado os arquivos __init__.py, json de testes e arquivo teste.py

---
+++ utils/teste.py
@@ -0,0 +1,3 @@
+import restaurante
+print(f"A distância entre São Paulo e Rio de janeiro em Km é: {restaurante.proximidade_restaurante_geopy((23.550278, -46.633889), (22.902778, -43.207778))}")
+print(f"A distância entre São Paulo e Rio de janeiro em Km é: {restaurante.proximidade_restaurante((23.550278, -46.633889), (22.902778, -43.207778))}")

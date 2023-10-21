9545bb4b640de1a00f1ddeebd3d844c8791a5cc6
Matheus Acer <matheusmibrandao@gmail.com>
2023-10-21 09:27:17 -0300

    Refactor

--- utils/restaurante.py
+++ utils/restaurante.py
@@ -2,21 +2,22 @@ import math
 import utils
 from geopy import distance
 
-def cadastrar_restaurante(nome: str, coordenadas: list, n_pedidos=0):
+
+def cadastrar_restaurante(nome: str, coordenadas: list, n_pedidos=0) -> dict:
     cardapio = []
     cardapio = criar_cardapio(cardapio)
-    restaurante = { 'nome': nome, 
-                   'lat': coordenadas[0], 
-                   'lon': coordenadas[1], 
-                   'qtd_pedidos': n_pedidos,
-                   'cardapio': cardapio
-                   }
+    restaurante = {
+        "nome": nome,
+        "lat": coordenadas[0],
+        "lon": coordenadas[1],
+        "qtd_pedidos": n_pedidos,
+        "cardapio": cardapio,
+    }
     return restaurante
 
 
 def proximidade_restaurante(coord_1: list, coord_2: list) -> float:
-    
-    raio_terra = 6371.0 # raio da terra em km
+    raio_terra = 6371.0  # raio da terra em km
 
     # Converter graus para radianos
     lat1 = math.radians(coord_1[0])
@@ -25,55 +26,61 @@ def proximidade_restaurante(coord_1: list, coord_2: list) -> float:
     lon2 = math.radians(coord_2[1])
 
     # Fórmula de Haversine
-    a = math.sin((lat2 - lat1)/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin((lon2 - lon1)/2)**2
+    a = (
+        math.sin((lat2 - lat1) / 2) ** 2
+        + math.cos(lat1) * math.cos(lat2) * math.sin((lon2 - lon1) / 2) ** 2
+    )
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
     # Distância em quilômetros
-    distance = round(raio_terra*c, 2)
+    distance = round(raio_terra * c, 2)
 
     return distance
 
-#abordagem usando a geopy 
-def proximidade_restaurante_geopy(coord_1: list, coord_2: list) -> float:    
+
+# abordagem usando a geopy
+def proximidade_restaurante_geopy(coord_1: list, coord_2: list) -> float:
     return round(distance.distance(coord_1, coord_2).km, 2)
 
-def criar_cardapio(items: list) -> None:
+
+def criar_cardapio(items: list) -> list:
     print("Digite os itens do cardápio (digite 'sair' para terminar):")
     while True:
-        '''
+        """
         item_cardapio = {
-            'nome_prouto': str,
+            'nome_produto': str,
             'categoria': str,
             'alcoolico': str,
             'valor': float,
             'estoque': int,
         }
-        '''
-        nome_prouto = input("Nome do produto: ")
-        if nome_prouto.lower() == 'sair':
+        """
+        nome_produto = input("Nome do produto: ")
+        if nome_produto.lower() == "sair":
             break
         categoria = input("Tipo do item (comida ou bebida): ").lower()
         alcoolico = bool(input("O item é alcoolico?: 1 - Sim, 0 - Não").lower())
         valor = float(input("Informe o valor do produto: "))
-        estoque = int(input('Informe a quantidade em estoque: '))
+        estoque = int(input("Informe a quantidade em estoque: "))
         items.append(
             {
-            'nome_prouto': nome_prouto,
-            'categoria': categoria,
-            'alcoolico': alcoolico,
-            'valor': valor,
-            'estoque': estoque,
+                "nome_produto": nome_produto,
+                "categoria": categoria,
+                "alcoolico": alcoolico,
+                "valor": valor,
+                "estoque": estoque,
             }
         )
     return items
 
-def fazer_pedido(cardapio, pedido):
+
+def fazer_pedido(cardapio, pedido) -> None:
     item = pedido
     if item in cardapio:
         print(f"Adicionando {item} ao seu pedido.")
         pedido.append(item)
 
-def registrar_vendas(self, vendas=None):
+def registrar_vendas(self, vendas=None) -> None:
     print("Digite a quantidade de vendas:")
     try:
         vendas = int(input())
